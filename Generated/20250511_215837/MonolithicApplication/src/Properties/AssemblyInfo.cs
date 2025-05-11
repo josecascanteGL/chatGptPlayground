5a6ca2cc8ab9ce01cb294571d17736a1b658aaec
Jose Luis Cascante <jose.cascante@gorillalogic.com>
2025-05-11 15:59:49 -0600

    Generated from ChatGPT

---
+++ Generated/20250511_215837/MonolithicApplication/src/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+// Set the title of the assembly to "UnicornShopLegacy"
+[assembly: AssemblyTitle("UnicornShopLegacy")]
+
+// Set the description of the assembly to an empty string
+[assembly: AssemblyDescription("")]
+
+// Set the configuration of the assembly to an empty string
+[assembly: AssemblyConfiguration("")]
+
+// Set the company name associated with the assembly to an empty string
+[assembly: AssemblyCompany("")]
+
+// Set the product name of the assembly to "UnicornShopLegacy"
+[assembly: AssemblyProduct("UnicornShopLegacy")]
+
+// Set the copyright information of the assembly to the year 2020
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+
+// Set the trademark information of the assembly to an empty string
+[assembly: AssemblyTrademark("")]
+
+// Set the assembly culture to an empty string
+[assembly: AssemblyCulture("")]
+
+// Set the ComVisible attribute to false, indicating that the types in this assembly are not visible to COM components
+[assembly: ComVisible(false)]
+
+// Set the GUID (Globally Unique Identifier) for the typelib of the project if it is exposed to COM
+[assembly: Guid("eb42d2a2-5eef-4f7c-a111-5f93e534f155")]
+
+// Specify the version information for an assembly with values for Major Version, Minor Version, Build Number, and Revision
+// Using default values of 1.0.0.0 for AssemblyVersion
+[assembly: AssemblyVersion("1.0.0.0")]
+
+// Using default values of 1.0.0.0 for AssemblyFileVersion
+[assembly: AssemblyFileVersion("1.0.0.0")]
