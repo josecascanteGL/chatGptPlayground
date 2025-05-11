b8d9ee40d6fef53b06699161f07dad7d6f3394af
Jose Luis Cascante <jose.cascante@gorillalogic.com>
2025-05-10 22:55:04 -0600

    Delete Generated/20250511_044710/MonolithicApplication directory

--- Generated/20250511_044710/MonolithicApplication/src/Interfaces/IUnishopEntities.cs
+++
@@ -1,6 +0,0 @@
-// Copyright and license information
-
-// Import necessary namespaces
-// Define the interface IUnishopEntities that extends IDisposable
-// The interface contains properties for DbSet of user, inventory, and basket
-// Also contains methods for DbEntityEntry, SaveChangesAsync, and SetModified.
