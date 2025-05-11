d7a2a7d44949100bb64142eeec9822fe5f15a35f
Jose Luis Cascante <jose.cascante@gorillalogic.com>
2025-05-11 15:16:55 -0600

    Generated from ChatGPT

---
+++ Generated/20250511_210833/MonolithicApplication/test/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+// Set the title of the assembly to "UnicornStoreLegacy.Tests"
+[assembly: AssemblyTitle("UnicornStoreLegacy.Tests")]
+
+// Set the description of the assembly to an empty string
+[assembly: AssemblyDescription("")]
+
+// Set the configuration of the assembly to an empty string
+[assembly: AssemblyConfiguration("")]
+
+// Set the company of the assembly to an empty string
+[assembly: AssemblyCompany("")]
+
+// Set the product of the assembly to "UnicornStoreLegacy.Tests"
+[assembly: AssemblyProduct("UnicornStoreLegacy.Tests")]
+
+// Set the copyright of the assembly to "Copyright ©  2020"
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+
+// Set the trademark of the assembly to an empty string
+[assembly: AssemblyTrademark("")]
+
+// Set the culture of the assembly to an empty string
+[assembly: AssemblyCulture("")]
+
+// Set the assembly as not ComVisible
+[assembly: ComVisible(false)]
+
+// Set the GUID of the assembly to "965ef1f3-f9ca-49b2-a0ed-9757498462df"
+[assembly: Guid("965ef1f3-f9ca-49b2-a0ed-9757498462df")]
+
+// Set the version of the assembly to "1.0.0.0"
+[assembly: AssemblyVersion("1.0.0.0")]
+
+// Set the file version of the assembly to "1.0.0.0"
+[assembly: AssemblyFileVersion("1.0.0.0")]
