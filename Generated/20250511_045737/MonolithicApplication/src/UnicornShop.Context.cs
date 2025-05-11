0952c59cede66e016fdd0be78b8a68b828a43ead
Jose Luis Cascante <jose.cascante@gorillalogic.com>
2025-05-10 23:29:59 -0600

    Delete Generated/20250511_045737 directory

--- Generated/20250511_045737/MonolithicApplication/src/UnicornShop.Context.cs
+++
@@ -1,8 +0,0 @@
-// This code is part of the UnicornShopLegacy namespace, which contains the UnishopEntities class.
-// The UnishopEntities class inherits from DbContext, which is part of Entity Framework, for managing a database context.
-
-// The constructor UnishopEntities initializes the UnishopEntities context with a connection string named "UnishopEntities".
-
-// The OnModelCreating method is overridden to throw an UnintentionalCodeFirstException if it's called. This prevents accidental code-first migrations.
-
-// The UnishopEntities class has DbSets for three entities: baskets, users, and inventories. These properties represent tables in the database.
--- Generated/20250511_045737/MonolithicApplication/src/inventory.cs
+++
@@ -1,6 +0,0 @@
-// The code is defining a partial class 'inventory' in the namespace 'UnicornShopLegacy'
-// The class has several properties - 'unicorn_id', 'name', 'description', 'price', 'image', 'date_create'
-// The 'unicorn_id' property is of type System.Guid
-// The 'name', 'description', 'image' properties are of type string
-// The 'price' property is of type Nullable<decimal>, meaning it can store a decimal value or null
-// The 'date_create' property is of type Nullable<System.DateTime>, meaning it can store a DateTime value or null
