cea033e85b6e19b83ac0c1396ed368d57a61881c
Jose Luis Cascante <jose.cascante@gorillalogic.com>
2025-05-10 19:53:54 -0600

    Generated from ChatGPT

---
+++ Generated/20250511_015315/MonolithicApplication/src/Classes/UnishopEntities.cs
@@ -0,0 +1,5 @@
+// Including necessary using directives
+// UnishopEntities class implements the IUnishopEntities interface
+// Partial class is used to allow separate implementations in different files
+
+// SetModified method takes an object entity as input and marks its state as modified in the entity framework context.
