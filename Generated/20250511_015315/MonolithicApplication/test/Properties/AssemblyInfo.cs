5665510539086f75114e37bde437785f55773272
Jose Luis Cascante <jose.cascante@gorillalogic.com>
2025-05-10 19:55:43 -0600

    Generated from ChatGPT

---
+++ Generated/20250511_015315/MonolithicApplication/test/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+// Using System.Reflection to access Assembly information
+// Using System.Runtime.InteropServices for interacting with COM components
+
+// Setting the title of the assembly to "UnicornStoreLegacy.Tests"
+[assembly: AssemblyTitle("UnicornStoreLegacy.Tests")]
+
+// Setting the description of the assembly to an empty string
+[assembly: AssemblyDescription("")]
+
+// Setting the configuration of the assembly to an empty string
+[assembly: AssemblyConfiguration("")]
+
+// Setting the company of the assembly to an empty string
+[assembly: AssemblyCompany("")]
+
+// Setting the product of the assembly to "UnicornStoreLegacy.Tests"
+[assembly: AssemblyProduct("UnicornStoreLegacy.Tests")]
+
+// Setting the copyright information of the assembly to 2020
+[assembly: AssemblyCopyright("Copyright © 2020")]
+
+// Setting the trademark of the assembly to an empty string
+[assembly: AssemblyTrademark("")]
+
+// Setting the culture of the assembly to an empty string
+[assembly: AssemblyCulture("")]
+
+// Making the assembly not visible to COM components
+[assembly: ComVisible(false)]
+
+// Setting the GUID (Globally Unique Identifier) of the assembly
+[assembly: Guid("965ef1f3-f9ca-49b2-a0ed-9757498462df")]
+
+// Setting the version of the assembly to 1.0.0.0
+[assembly: AssemblyVersion("1.0.0.0")]
+
+// Setting the file version of the assembly to 1.0.0.0
+[assembly: AssemblyFileVersion("1.0.0.0")]
