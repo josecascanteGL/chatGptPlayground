3ec0bac00c14cb9b106a5904c1af25c7f034d5cc
Jose Luis Cascante <jose.cascante@gorillalogic.com>
2025-05-11 16:06:38 -0600

    Generated from ChatGPT

---
+++ Generated/20250511_220531/MonolithicApplication/src/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+// Set the title of the assembly to "UnicornShopLegacy"
+[assembly: AssemblyTitle("UnicornShopLegacy")]
+
+// Set the description of the assembly to an empty string
+[assembly: AssemblyDescription("")]
+
+// Set the configuration of the assembly to an empty string
+[assembly: AssemblyConfiguration("")]
+
+// Set the company of the assembly to an empty string
+[assembly: AssemblyCompany("")]
+
+// Set the product name of the assembly to "UnicornShopLegacy"
+[assembly: AssemblyProduct("UnicornShopLegacy")]
+
+// Set the copyright information of the assembly to "Copyright © 2020"
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+
+// Set the assembly's trademark information to an empty string
+[assembly: AssemblyTrademark("")]
+
+// Set the assembly's culture to an empty string
+[assembly: AssemblyCulture("")]
+
+// Set ComVisible to false to make the types in this assembly not visible to COM components
+[assembly: ComVisible(false)]
+
+// Set the GUID of the typelib if this project is exposed to COM
+[assembly: Guid("eb42d2a2-5eef-4f7c-a111-5f93e534f155")]
+
+// Set the assembly's version number to "1.0.0.0"
+[assembly: AssemblyVersion("1.0.0.0")]
+
+// Set the assembly's file version to "1.0.0.0"
+[assembly: AssemblyFileVersion("1.0.0.0")
