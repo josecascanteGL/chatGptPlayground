e0bb4e2417c8dd1e8b0923a792d19bfa7a66d724
Jose Luis Cascante <jose.cascante@gorillalogic.com>
2025-05-11 16:06:03 -0600

    Generated from ChatGPT

---
+++ Generated/20250511_220531/MonolithicApplication/src/Classes/UnishopEntities.cs
@@ -0,0 +1,6 @@
+/*
+* This code is a partial class that implements the IUnishopEntities interface.
+* It contains a method SetModified that takes an object entity as a parameter.
+* Within the method, it sets the state of the entity to EntityState.Modified using the Entry method of the current UnishopEntities instance.
+* The purpose of this class is to provide a mockable method instead of directly using the Entry method.
+*/
