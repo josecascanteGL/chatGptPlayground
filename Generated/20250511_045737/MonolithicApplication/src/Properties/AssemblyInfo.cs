0b850f6fddc5a98b70416c5733fa73b3b6e594a2
Jose Luis Cascante <jose.cascante@gorillalogic.com>
2025-05-10 22:58:53 -0600

    Generated from ChatGPT

---
+++ Generated/20250511_045737/MonolithicApplication/src/Properties/AssemblyInfo.cs
@@ -0,0 +1,48 @@
+// These lines import necessary libraries for the assembly meta-information
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+// The AssemblyTitle attribute specifies the title of the assembly
+[assembly: AssemblyTitle("UnicornShopLegacy")]
+// The AssemblyDescription attribute describes the purpose or content of the assembly
+[assembly: AssemblyDescription("")]
+// The AssemblyConfiguration attribute specifies any configuration information for the assembly
+[assembly: AssemblyConfiguration("")]
+// The AssemblyCompany attribute specifies the company responsible for the assembly
+[assembly: AssemblyCompany("")]
+// The AssemblyProduct attribute specifies the overall product that the assembly belongs to
+[assembly: AssemblyProduct("UnicornShopLegacy")]
+// The AssemblyCopyright attribute specifies the copyright information
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+// The AssemblyTrademark attribute specifies any trademarks associated with the assembly
+[assembly: AssemblyTrademark("")]
+// The AssemblyCulture attribute specifies the culture information for the assembly
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+// The ComVisible attribute controls whether types in this assembly can be accessed by COM components
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+// The Guid attribute specifies a unique identifier for the assembly if exposed to COM
+[assembly: Guid("eb42d2a2-5eef-4f7c-a111-5f93e534f155")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+// The AssemblyVersion attribute specifies the version of the assembly
+[assembly: AssemblyVersion("1.0.0.0")]
+// The AssemblyFileVersion attribute specifies the version of the file
+[assembly: AssemblyFileVersion("1.0.0.0")]
