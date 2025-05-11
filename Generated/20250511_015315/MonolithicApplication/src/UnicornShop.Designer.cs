25e60fb8f47bd685d02345b5766dcf7c6da42d5a
Jose Luis Cascante <jose.cascante@gorillalogic.com>
2025-05-10 19:53:31 -0600

    Generated from ChatGPT

---
+++ Generated/20250511_015315/MonolithicApplication/src/UnicornShop.Designer.cs
@@ -0,0 +1,7 @@
+// This block of comments explains the use of T4 code generation for the model in the specified file path.
+
+// The following comments provide instructions on how to enable legacy code generation and change the Code Generation Strategy property.
+
+// If no context and entity classes have been generated, it may be due to creating an empty model without selecting an Entity Framework version.
+
+// Instructions on how to generate context class and entity classes for the model by updating it from the database or generating the database from the model.
