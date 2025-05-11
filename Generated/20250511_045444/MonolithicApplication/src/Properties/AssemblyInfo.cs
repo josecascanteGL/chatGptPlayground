a0fb45c8ecf69a009bd43300610fe2404d7abfdc
Jose Luis Cascante <jose.cascante@gorillalogic.com>
2025-05-10 22:55:54 -0600

    Generated from ChatGPT

---
+++ Generated/20250511_045444/MonolithicApplication/src/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+// Title of the assembly
+[assembly: AssemblyTitle("UnicornShopLegacy")]
+
+// Description of the assembly
+[assembly: AssemblyDescription("")]
+
+// Configuration of the assembly
+[assembly: AssemblyConfiguration("")]
+
+// Company associated with the assembly
+[assembly: AssemblyCompany("")]
+
+// Product name of the assembly
+[assembly: AssemblyProduct("UnicornShopLegacy")]
+
+// Copyright information for the assembly
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+
+// Trademark information for the assembly
+[assembly: AssemblyTrademark("")]
+
+// Culture associated with the assembly
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM components.
+// If you need to access a type in this assembly from COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// GUID for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("eb42d2a2-5eef-4f7c-a111-5f93e534f155")]
+
+// Version information for an assembly consists of the following four values:
+// Major Version
+// Minor Version
+// Build Number
+// Revision
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")
