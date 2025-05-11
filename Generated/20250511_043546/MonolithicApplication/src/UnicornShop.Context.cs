81db63ee5c07d8a066246878cb46233732641852
Jose Luis Cascante <jose.cascante@gorillalogic.com>
2025-05-10 22:47:06 -0600

    Delete Generated directory

--- Generated/20250511_043546/MonolithicApplication/src/UnicornShop.Context.cs
+++
@@ -1,5 +0,0 @@
-// This code is defining the structure of the UnishopEntities class which is a DbContext in Entity Framework.
-// The constructor initializes the UnishopEntities class with the connection string "UnishopEntities".
-// The OnModelCreating method is overridden to throw an UnintentionalCodeFirstException, preventing accidental code-first migrations.
-// Three DbSets are defined for the entities: basket, user, and inventory.
-// These DbSets will represent tables in the database and allow querying and manipulating the data.
--- Generated/20250511_044550/MonolithicApplication/src/app_user.cs
+++
@@ -1,3 +0,0 @@
-// This code defines a partial class called app_user within the UnicornShopLegacy namespace
-// The class represents a user entity with properties for user_id, active, email, first_name, last_name, and password
-// The properties are defined with appropriate data types and allow setting and getting their values.
