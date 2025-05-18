7692d3a33f237801f53dc84ad9b0d164d0ad3775
Jose Luis Cascante <jose.cascante@gorillalogic.com>
2025-05-18 11:04:34 -0600

    Generated from ChatGPT

---
+++ Generated/20250518_170346/Console-Samples/010-StringComparison/StringComparison.dpr
@@ -0,0 +1,114 @@
+// Main program declaration with the name StringComparison
+program StringComparison;
+
+// Compiler directive to define application type as console application
+{$APPTYPE CONSOLE}
+// Compiler directive to include resource files in the build process
+{$R *.res}
+
+// Using namespace for system utilities, standard library.
+uses
+  System.SysUtils;
+
+// Variable declaration for multiple string variables to illustrate different capitalizations
+var
+  s1, s1L, s1U, s1LI, s1UI, s2, s2L, s2U, s2LI, s2UI, s3, s3L, s3U, s3LI, s3UI: string;
+
+// Beginning of main program block
+begin
+  // Exception handling begin block
+  try
+    // Set initial strings with different cases
+    s1 := 'IciOuPas';
+    s1L := s1.ToLower; // Convert to lowercase
+    s1LI := s1.ToLowerInvariant; // Convert to lowercase using invariant culture
+    s1U := s1.ToUpper; // Convert to uppercase
+    s1UI := s1.ToUpperInvariant; // Convert to uppercase using invariant culture
+
+    // Repeat the process for second string
+    s2 := 'icioupas';
+    s2L := s2.ToLower;
+    s2LI := s2.ToLowerInvariant;
+    s2U := s2.ToUpper;
+    s2UI := s2.ToUpperInvariant;
+
+    // Repeat the process for third string
+    s3 := 'ICIOUPAS';
+    s3L := s3.ToLower;
+    s3LI := s3.ToLowerInvariant;
+    s3U := s3.ToUpper;
+    s3UI := s3.ToUpperInvariant;
+
+    // Output the results of these transformations
+    writeln('s1');
+    writeln(s1);
+    writeln(s1L);
+    writeln(s1LI);
+    writeln(s1U);
+    writeln(s1UI);
+
+    writeln('s2');
+    writeln(s2);
+    writeln(s2L);
+    writeln(s2LI);
+    writeln(s2U);
+    writeln(s2UI);
+
+    writeln('s3');
+    writeln(s3);
+    writeln(s3L);
+    writeln(s3LI);
+    writeln(s3U);
+    writeln(s3UI);
+
+    // Compare strings using '=' operator which is case-sensitive
+    writeln('s1 = s2');
+    writeln(s1 = s2);
+    writeln(s1L = s2L);
+    writeln(s1U = s2U);
+    writeln(s1LI = s2LI);
+    writeln(s1UI = s2UI);
+
+    writeln('s1 = s3');
+    writeln(s1 = s3);
+    writeln(s1L = s3L);
+    writeln(s1U = s3U);
+    writeln(s1LI = s3LI);
+    writeln(s1UI = s3UI);
+
+    // Compare strings using CompareStr function which is case-sensitive
+    writeln('CompareStr');
+    writeln(compareStr(s1, s2));
+    writeln(compareStr(s1L, s2L));
+    writeln(compareStr(s1U, s2U));
+    writeln(compareStr(s1LI, s2LI));
+    writeln(compareStr(s1UI, s2UI));
+    writeln(compareStr(s1, s3));
+    writeln(compareStr(s1L, s3L));
+    writeln(compareStr(s1U, s3U));
+    writeln(compareStr(s1LI, s3LI));
+    writeln(compareStr(s1UI, s3UI));
+
+    // Compare strings using CompareText function which is case-insensitive
+    writeln('CompareText');
+    writeln(compareText(s1, s2));
+    writeln(compareText(s1L, s2L));
+    writeln(compareText(s1U, s2U));
+    writeln(compareText(s1LI, s2LI));
+    writeln(compareText(s1UI, s2UI));
+    writeln(compareText(s1, s3));
+    writeln(compareText(s1L, s3L));
+    writeln(compareText(s1U, s3U));
+    writeln(compareText(s1LI, s3LI));
+    writeln(compareText(s1UI, s3UI));
+
+    // Command to keep the console window open to view the results
+    readln;
+  // Exception handling end block
+  except
+    // Capture and report any exceptions that occur
+    on E: Exception do
+      writeln(E.ClassName, ': ', E.Message);
+  end;
+
+end.  // End of program block
